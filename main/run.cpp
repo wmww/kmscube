095fb542702c6a63b4a2c9b6f4e23cbe29c1e981
William W Wold <wilcoze@gmail.com>
2017-09-19 21:06:22 -0700

    it fucking works!

--- main/run.cpp
+++ main/run.cpp
@@ -4,21 +4,33 @@
 #include "../common.h"
 #include <GL/gl.h>
 
+// change to toggle debug statements on and off
+#define debug debug_on
+
 struct
 {
 	struct egl egl;
+	Texture texture;
+	shared_ptr<RectRenderer> rectRenderer;
 }
 data;
+
+void draw(unsigned i)
+{
+	debug(FUNC + " called");
+	ASSERT(data.texture.isValid());
+	ASSERT(data.rectRenderer);
+	data.rectRenderer->draw(data.texture, V2d(0, 0), V2d(1, 1));
+}
 
 const struct egl * init_egl_view(const struct gbm *gbm)
 {
 	int ret = init_egl(&data.egl, gbm);
 	ASSERT_ELSE(ret == 0, return nullptr);
 	glViewport(0, 0, gbm->width, gbm->height);
-	Texture texture;
-	texture.loadFromImage("assets/hedgehog.jpg");
-	RectRenderer rectRenderer;
-	//rectRenderer.draw(texture, V2d(0, 0), V2d(1, 1));
+	data.texture.loadFromImage("assets/hedgehog.jpg");
+	data.rectRenderer = make_shared<RectRenderer>();
+	data.egl.draw = draw;
 	return &data.egl;
 }
 
